be790a7d23300e712beac183405a2d61f8c55211
varshakharolia <varshakharolia2@gmail.com>
2024-04-03 09:29:55 +0530

    wether

---
+++ src/app/Environment/EnvironmentVariables.ts
@@ -0,0 +1,16 @@
+export const EnvironmentalVariables = {
+    production:false,
+
+    //Base Urls to access the end points using the http client get method
+    weatherApiLocationBaseURL:'https://weather338.p.rapidapi.com/locations/search?',
+    weatherApiForecastBaseURL:'https://weather338.p.rapidapi.com/weather/forecast?',
+    
+    //Variables for API key name and value
+    xRapidApiKeyName:'X-RapidAPI-Key',
+    xRapidApikeyValue:'5e770a4c1fmsh2b46b386b4e82f5p1eaa36jsn3aa0607cc064',
+
+    //Variables for host name and value
+    xRapidApiHostName:'X-RapidAPI-Host',
+    xRapidApiHostValue:'weather338.p.rapidapi.com'
+    
+}
